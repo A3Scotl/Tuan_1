e12fe1b1741bb391648b1110679af05e9c1ee0fa
H51M14\Student <Student@gmail.com>
2024-08-26 17:06:23 +0700

    update code c3

--- Part2/challenge3.js
+++ Part2/challenge3.js
@@ -12,3 +12,18 @@ var john = new Person("John", 92, 1.95);
 var marks = new Person("Marks", 78, 1.69);
 console.log(marks);
 console.log(john);
+
+function checkHeights() {
+    if (john.mbi > marks.mbi)(
+        console.log(`John's BMI (${john.mbi}) is higher than Mark's (${marks.mbi})!`)
+    )
+    else if (john.mbi > marks.mbi) {
+        console.log(`Johns BMI (${john.mbi}) = Marks (${marks.mbi})`)
+
+    } else {
+        console.log(`Mark's (${marks.mbi}) is higher than John's BMI (${john.mbi})!`)
+
+
+    }
+}
+checkHeights()
